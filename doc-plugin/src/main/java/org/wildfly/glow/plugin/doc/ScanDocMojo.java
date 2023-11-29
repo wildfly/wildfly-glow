3e5cb2ad73fc147dedb74c1c5436955b1e5f9ccc
Jean-Francois Denise <jdenise@redhat.com>
2023-11-29 12:24:40 +0100

    Rule description contains rule classes only

--- doc-plugin/src/main/java/org/wildfly/glow/plugin/doc/ScanDocMojo.java
+++ doc-plugin/src/main/java/org/wildfly/glow/plugin/doc/ScanDocMojo.java
@@ -44,7 +44,6 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import org.jboss.galleon.config.FeaturePackConfig;
 import org.jboss.galleon.config.ProvisioningConfig;
 import org.jboss.galleon.layout.FeaturePackLayout;
@@ -53,6 +52,7 @@ import org.jboss.galleon.universe.UniverseResolver;
 import org.jboss.galleon.universe.maven.repo.MavenRepoManager;
 import org.jboss.galleon.xml.ProvisioningXmlParser;
 import org.wildfly.glow.FeaturePacks;
+import org.wildfly.glow.LayerMetadata;
 import org.wildfly.glow.Utils;
 
 /**
@@ -91,22 +91,21 @@ public class ScanDocMojo extends AbstractMojo {
             MavenRepoManager artifactResolver = new MavenArtifactRepositoryManager(repoSystem, repoSession, repositories);
             UniverseResolver universeResolver = UniverseResolver.builder().addArtifactResolver(artifactResolver).build();
             Map<Layer, Map<String, String>> rules = new TreeMap<>();
-            Set<String> ruleDescriptions = new TreeSet<>();
             StringBuilder rulesBuilder = new StringBuilder();
             Properties properties = new Properties();
             try (FileInputStream in = new FileInputStream(Paths.get(rulesPropertiesFile).toFile())) {
                 properties.load(in);
             }
 
-            getRules("bare-metal", universeResolver, rules, ruleDescriptions);
+            getRules("bare-metal", universeResolver, rules);
             Map<Layer, Map<String, String>> cloudRules = new TreeMap<>();
-            getRules("cloud", universeResolver, cloudRules, ruleDescriptions);
+            getRules("cloud", universeResolver, cloudRules);
 
             rulesBuilder.append("== [[glow.table.rules]]Rules descriptions\n");
             rulesBuilder.append("[cols=\"1,2,1\"]\n");
             rulesBuilder.append("|===\n");
             rulesBuilder.append("|Rule |Description |Value\n");
-            for (String k : ruleDescriptions) {
+            for (String k : LayerMetadata.getAllRules()) {
                 rulesBuilder.append("|[[glow." + k + "]]" + k + "\n");
                 String desc = properties.getProperty(k);
                 String val = properties.getProperty(k + ".value");
@@ -156,7 +155,11 @@ public class ScanDocMojo extends AbstractMojo {
             rulesBuilder.append("|\n");
             Map<String, String> local = rules.get(l);
             for (String k : local.keySet()) {
-                rulesBuilder.append("link:#glow." + k + "[" + k + "]" + "=" + local.get(k)).append(" +\n");
+                String ruleClass = LayerMetadata.getRuleClass(k);
+                if (ruleClass == null) {
+                    throw new Exception("Unknown rule " + k);
+                }
+                rulesBuilder.append("link:#glow." + ruleClass + "[" + k + "]" + "=" + local.get(k)).append(" +\n");
             }
             rulesBuilder.append("l|\n");
             for (FeaturePackLocation.FPID id : l.getFeaturePacks()) {
@@ -168,8 +171,7 @@ public class ScanDocMojo extends AbstractMojo {
     }
 
     private void getRules(String context, UniverseResolver universeResolver,
-            Map<Layer, Map<String, String>> rules,
-            Set<String> ruleDescriptions) throws Exception {
+            Map<Layer, Map<String, String>> rules) throws Exception {
         try (ProvisioningLayout<FeaturePackLayout> layout = Utils.buildLayout(context,
                 null, null, GlowMessageWriter.DEFAULT, false)) {
             Map<String, Layer> all;
@@ -184,9 +186,6 @@ public class ScanDocMojo extends AbstractMojo {
                 if (!l.getProperties().isEmpty()) {
                     Map<String, String> props = rules.computeIfAbsent(l, (value) -> new TreeMap<>());
                     props.putAll(l.getProperties());
-                    for (String k : l.getProperties().keySet()) {
-                        ruleDescriptions.add(k);
-                    }
                 }
             }
         }
